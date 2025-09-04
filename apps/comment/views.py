04d9c9a22daeadb536effe33e178bad05ebd76e6
sasha125588 <sashavolskiy11@gmail.com>
2025-09-04 22:35:19 +0300

    fix comment views

--- apps/comment/views.py
+++ apps/comment/views.py
@@ -18,7 +18,7 @@ from .serializers import (
 
 
 # Create your views here.
-class ComentListCreateView(generics.ListCreateAPIView):
+class CommentListCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filter_backends = [
         DjangoFilterBackend,
@@ -68,6 +68,25 @@ class CommentRepliesView(generics.ListAPIView):
         filters.OrderingFilter,
     ]
     filterset_fields = ["post", "is_active", "parent"]
+    search_fields = ["content"]
+    ordering_fields = ["created_at", "updated_at"]
+    ordering = ["-created_at"]
+
+    def get_queryset(self):
+        return Comment.objects.filter(author=self.request.user).select_related(
+            "post", "parent"
+        )
+
+
+class MyCommentsView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ["post", "parent", "is_active"]
     search_fields = ["content"]
     ordering_fields = ["created_at", "updated_at"]
     ordering = ["-created_at"]
